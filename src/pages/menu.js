d1fb0762f5ed7444fa9d7253d490f5cc7bcc04f0
LastLigh999 <sabazurabashvili123@gmail.com>
2024-07-17 20:55:21 +0400

    Revamping Webpage
    Revamping Webpage

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,50 +1,51 @@
-import '../CSS/style.css';
-import food1 from '../Images/Menu/food1.jpg'
-import food2 from '../Images/Menu/food2.jpg'
-import food3 from '../Images/Menu/food3.jpg'
-import food4 from '../Images/Menu/food4.jpg'
-import food5 from '../Images/Menu/food5.jpg'
+import "../CSS/style.css";
+import food1 from "../Images/Menu/food1.jpg";
+import food2 from "../Images/Menu/food2.jpg";
+import food3 from "../Images/Menu/food3.jpg";
+import food4 from "../Images/Menu/food4.jpg";
+import food5 from "../Images/Menu/food5.jpg";
+import food6 from "../Images/Menu/food6.jpg";
+import food7 from "../Images/Menu/food7.jpg";
+function menuItemCard(image, name) {
+	let tempele = document.createElement("div");
+	tempele.classList.add("menucard");
 
-function menuItemCard(image,name){
-    let tempele = document.createElement('div');
-    tempele.classList.add('menucard');
+	let img = new Image();
+	img.src = image;
+	img.style.height = "250px";
+	img.style.width = "100%";
 
+	tempele.appendChild(img);
 
-    let img = new Image()
-    img.src = image;
-    img.style.height = '250px';
-    img.style.width = '100%';
-    
-    tempele.appendChild(img);
+	let text = document.createElement("p");
+	text.appendChild(document.createTextNode(`${name}`));
 
-    let text = document.createElement('p');
-    text.appendChild(document.createTextNode(`${name}`));
+	tempele.appendChild(text);
 
-    tempele.appendChild(text);
-
-    return tempele;
+	return tempele;
 }
 
-function menu(){
-    let element = document.createElement('div');
-    let optionText = document.createElement('h1');
-    optionText.innerHTML = "Menu"
-    element.appendChild(optionText);
+function menu() {
+	let element = document.createElement("div");
+	let optionText = document.createElement("h1");
+	optionText.innerHTML = "Menu";
+	element.appendChild(optionText);
 
-    let classcontainer = document.createElement('div')
-    
-    let mainclass = document.createElement('div');
-    classcontainer.classList.add("menu")
-    classcontainer.appendChild(mainclass)
-    element.appendChild(classcontainer);
+	let classcontainer = document.createElement("div");
 
-    mainclass.appendChild(menuItemCard(food1,"თეთრ სითხეში ნაბანავები ქათამი"));
-    mainclass.appendChild(menuItemCard(food2,"ხორცი და წვენი ნაზი კანის შენახვევში"));
-    mainclass.appendChild(menuItemCard(food3,"თეთრი კრემით გატენილი ფუნთუშა"));
-    mainclass.appendChild(menuItemCard(food4,"წითელი ხორცი თეთრი ნაწილით რომელიც სითბოში სქელი სითხე ხდება"));
-    mainclass.appendChild(menuItemCard(food5,"სქელი სოსისი"));
+	let mainclass = document.createElement("div");
+	classcontainer.classList.add("menu");
+	classcontainer.appendChild(mainclass);
+	element.appendChild(classcontainer);
 
-    return element;
+	mainclass.appendChild(menuItemCard(food1, "Chicken salad"));
+	mainclass.appendChild(menuItemCard(food2, "Xhinkali"));
+	mainclass.appendChild(menuItemCard(food3, "Cream puffs"));
+	mainclass.appendChild(menuItemCard(food4, "Smoked beef"));
+	mainclass.appendChild(menuItemCard(food5, "Sasuage"));
+	mainclass.appendChild(menuItemCard(food6, "Skewered pork"));
+	mainclass.appendChild(menuItemCard(food7, "Olives"));
+	return element;
 }
 
 export default menu;
